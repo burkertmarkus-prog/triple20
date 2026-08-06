7b41d1dbac4b17ff91f9a6bba8cf1fc4cd074d7f
burkertmarkus-prog <burkert.markus@gmail.com>
2026-08-06 13:52:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION='triple20-shell-20260805-5';
-const DATA_CACHE='triple20-data-20260805-5';
+const CACHE_VERSION='triple20-shell-20260806-1';
+const DATA_CACHE='triple20-data-20260806-1';
 const APP_SHELL=[
   './','./index.html','./styles.css','./app.js','./pwa.js','./manifest.webmanifest','./shop-products.json',
   './icons/triple20-icon-192.png','./icons/triple20-icon-512.png','./icons/apple-touch-icon.png','./icons/triple20-icon.svg',
